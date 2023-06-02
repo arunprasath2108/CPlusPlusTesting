2b3f2994b7d7691527b428ecc03419e98b131d98
arunprasath2108 <117808162+arunprasath2108@users.noreply.github.com>
2023-06-02 17:35:45 +0530

    After changing the locking strategy in mutex.

--- PollSystem/src/Server.cpp
+++ PollSystem/src/Server.cpp
@@ -6,28 +6,26 @@
 #include <map>
 #include <thread>
 #include <mutex>
-std::mutex poll_mutex;
-
 
 void addPollOptions();
+void printPollChoice();
 int processPollOptions();
 int getNumberOfClients();
-void communicateWithClient(int);
-void printResult(int, int);
 void acceptConnection(int);
+void communicateWithServer(int);
+void printResult(int, int);
+std::string prepareMessageToSend();
 void sendMessage(int, std::string);
 void receiveMessage(int);
 void addPollCount(std::string);
-int checkDuplicateEntry(std::string);
-std::string prepareMessageToSend();
-void printPollChoice();
+void increaseCount(std::string, int);
 
-std::vector<std::string> poll_options;
-std::vector<int> poll_results;
+std::map<std::string, int> poll;
+std::map<std::string, std::mutex> poll_mutexes;
 int number_of_clients, clients_participated = 0;
 
 int main() {
-  
+
   int server_socket, client_socket, port_number;
   struct sockaddr_in server_address;
 
@@ -47,7 +45,7 @@ int main() {
       std::cerr << "Error on binding.\n";
       exit(1);
   }
-  
+
   //add polling choices
   addPollOptions();
 
@@ -58,6 +56,60 @@ int main() {
 
   printResult(number_of_clients, clients_participated);
   close(server_socket);
+
+}
+
+void addPollOptions() {
+
+  int optionCount = 0;
+  std::cout << "Enter the choices for poll" << std::endl;
+  std::string new_choice;
+
+  while(true) {
+    int user_input;
+    std::cout << "Option " << ++optionCount << std::endl;
+    std::getline(std::cin >> std::ws, new_choice);
+    poll[new_choice];
+    std::cout << "Enter 1 to add Choice" << std::endl;
+    std::cout << "Enter 0 to continue" << std::endl;
+    std::cin >> user_input;
+    if(user_input == 0) {
+        printPollChoice();
+        break;
+    }
+    else if(user_input < 1 || user_input > 1) {
+        user_input = processPollOptions();
+        if(user_input == 0) {
+            std::cout << "Program terminated by user." << std::endl;
+            exit(1);
+        }
+        else if(user_input == 2) {
+            //for adding new options
+        }
+        else {
+            break;
+        }
+    }
+  }
+}
+
+int processPollOptions() {
+    int loopControl;
+    std::cout << std::endl;
+    std::cout << "Do you want to continue :\n";
+    std::cout << "Enter 1 for continue with options\n";
+    std::cout << "Enter 2 for adding options\n";
+    std::cout << "Enter 0 for exit the process\n";
+    std::cin >> loopControl;
+    return loopControl;
+}
+
+void printPollChoice() {
+    int count = 0;
+    std::cout << "Poll Choices: " << std::endl;
+    for(auto it: poll) {
+        std::cout << ++count << " - " << it.first << std::endl;
+    }
 }
 
 int getNumberOfClients() {
@@ -65,12 +117,13 @@ int getNumberOfClients() {
     int clients_count = 0;
     std::cout << "Enter number of clients to participate.." << std::endl; 
     std::cin >> clients_count;
-
     if(clients_count > 0) {
         return clients_count;
     }
     else {
         std::cout << "Enter a Valid Input" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
         return getNumberOfClients();
     }
 }
@@ -87,8 +140,6 @@ void acceptConnection(int server_socket) {
     while(true) {
         client_length = sizeof(client_address);
         client_socket = accept(server_socket, (struct sockaddr *) &client_address, &client_length);
-
-        //count the participants to check how many clients can participate
         clients_participated++;
         if(clients_participated > number_of_clients) {
             sendMessage(client_socket, "Poll has ended.");
@@ -96,79 +147,14 @@ void acceptConnection(int server_socket) {
             close(client_socket);
             break;
         }
-
-        std::thread clientThread(communicateWithClient, client_socket);
+        std::thread clientThread(communicateWithServer, client_socket);
         if(clientThread.joinable()) {
             clientThread.detach();
         }
     }
 }
 
-void addPollOptions() {
-
-    int loop_control = 1, optionsCount = 1;
-    std::cout << "Enter the choices for poll" << std::endl;
-    std::string option_entry;
-
-    while(loop_control) {
-        int user_choice = 0;
-        option_entry = "";
-        std::cout << "Option " << optionsCount++ << std::endl;
-        std::getline(std::cin >> std::ws, option_entry);
-
-        int isDuplicate = checkDuplicateEntry(option_entry);
-        if(isDuplicate == 1) {
-            poll_options.push_back(option_entry);
-        } else {
-            optionsCount--;
-            std::cout << "This choice is already available in list." << std::endl;
-        }
-
-        std::cout << "Enter 1 to add Choice" << std::endl;
-        std::cout << "Enter 0 to continue" << std::endl;
-        std::cin >> user_choice;
-        loop_control = user_choice;
-
-        if(loop_control != 1) {
-            printPollChoice();
-            user_choice = processPollOptions();
-
-            if(user_choice == 1) {
-                loop_control = 0;
-            } else if(user_choice == 2) {
-                loop_control = 1;
-            } else if(user_choice == 0) {
-                std::cout << "Program Terminated" << std::endl;
-                exit(1);
-            } else {
-                loop_control = 1;
-            }
-        }
-    }
-}
-
-int checkDuplicateEntry(std::string newEntry) {
-
-    for(int i=0; i<poll_options.size(); i++) {
-        if(poll_options[i] == newEntry) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int processPollOptions() {
-    int loopControl;
-    std::cout << std::endl;
-    std::cout << "Do you want to continue :\n";
-    std::cout << "Enter 1 for continue with options\n";
-    std::cout << "Enter 2 for adding options\n";
-    std::cout << "Enter 0 for exit the process\n";
-    std::cin >> loopControl;
-    return loopControl;
-}
-
-void communicateWithClient(int client_socket) {
+void communicateWithServer(int client_socket) {
 
     if (client_socket < 0) {
       std::cerr << "Error on accept.\n";
@@ -183,10 +169,9 @@ void communicateWithClient(int client_socket) {
 std::string prepareMessageToSend() {
 
     std::string message_to_send = "Poll your choice: ";
-    for(int i=0; i<poll_options.size(); i++) {
-        message_to_send.append("\n" + poll_options[i] + " ");
+    for(auto it : poll) {
+        message_to_send.append("\n" + it.first);
     }
-
     return message_to_send;
 }
 
@@ -199,52 +184,50 @@ void sendMessage(int client_socket, std::string message_to_send) {
 }
 
 void receiveMessage(int client_socket) {
-
     int bytes_read;
     std::string incoming_message;
     bytes_read = read(client_socket, &incoming_message, 255);
-    if (bytes_read < 0) {
-        std::cerr << "Error reading from socket.\n";
-        close(client_socket);
-    }
-    else {
-        std::cout << "Client choice : " << incoming_message << std::endl;
-        addPollCount(incoming_message);
-    }
+        if (bytes_read < 0) {
+            std::cerr << "Error reading from socket.\n";
+            close(client_socket);
+        }
+        else {
+            std::cout << "Client choice : " << incoming_message << std::endl;
+            addPollCount(incoming_message);
+        }
 }
 
 void addPollCount(std::string incoming_message) {
+    
+    if(poll.find(incoming_message) != poll.end()) {
+        int count = poll[incoming_message];
+        increaseCount(incoming_message, ++count);
+    }
+}
 
-    int index = std::stoi(incoming_message);
-     //if the list has value, else add new value to the index
-    if(poll_results.size() >= index) {
-        index--;
-        poll_results[index]++;
-    }
-    else {
-        poll_results.resize(index);
-        index--;
-        poll_results[index] = 1;
-    }
+void increaseCount(std::string incoming_message, int count) {
+    
+    std::lock_guard<std::mutex> lock(poll_mutexes[incoming_message]);
+    poll[incoming_message] = count;
 }
 
 void printResult(int clients, int participants) {
 
-    std::cout << "\nPoll Result: " << std::endl;
+    std::cout << "\nResult: " << std::endl;
     std::cout << "Total number of participants: " << clients << std::endl;
     std::cout << "Participated clients: " << participants << std::endl;
+    for(auto it : poll) {
+        std::cout << it.first << " " << it.second << std::endl;
+    }
+}
+
+
+
+
+
+
     
-    for(int i=0; i<poll_options.size(); i++) {
-        std::cout << poll_options[i] << " - " << poll_results[i] << std::endl; 
-    }
-}
 
-void printPollChoice() {
 
-    std::cout << "\nPoll Choices : \n";
-    std::cout << "---------------------\n";
-    for(int i=0; i<poll_options.size(); i++) {
-        std::cout << ++i << " " << poll_options[--i] << std::endl;
-    }
-}
+
     
